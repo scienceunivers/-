9c85c8f096cb6f4cf1ae5b26576f2a54084e46fc
scienceunivers <57838591+scienceunivers@users.noreply.github.com>
2020-02-17 23:22:39 +0800

    Create creditSharing_queryTable.py

---
+++ creditSharing_queryTable.py
@@ -0,0 +1,10 @@
+import os, sys
+import pandas as pd, numpy as np
+import datetime as dt
+import time
+import random
+import xlrd
+# openpyxl xlwt xlwriter xlrd等的比较，见
+# https://zhuanlan.zhihu.com/p/23998083
+# https://www.pythonexcel.com/
+# openpyxl, 功能全面，可读可写，对格式也有一定的操作，xlwriter对格式操作支持较好，pandas也可以写excel。
