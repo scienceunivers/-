2989497c5da5dae0803097fd0ecfdba300995673
scienceunivers <57838591+scienceunivers@users.noreply.github.com>
2021-02-21 17:46:58 +0800

    Update and rename 1_queryTableReading.py to 1_queryBookReading.py

--- 1_queryBookReading.py
+++ 1_queryBookReading.py
@@ -91,3 +91,56 @@ for index in df2.index:
     queryTable_all.loc[index,'更新日期']=df2.loc[index,'更新日期'].date()
 print(queryTable_all.loc[(queryTable_all['更新日期']!='#N/A')&(queryTable_all['更新日期']!=''),['更新日期']].shape)
 # 日志用logging不如print。
+
+
+
+
+
+# 其他工作表的读取，这部分可以考虑去掉。需求逐渐消失。
+
+objectiveSheet = 'zdjgzjdxx'
+
+nRows, nCols = workbook_query.sheet_by_name(objectiveSheet).nrows, workbook_query.sheet_by_name(objectiveSheet).ncols
+colNames = []
+workbook_queryDict = {}
+for i in range(0, nCols):
+    colNames.append(workbook_query.sheet_by_name(objectiveSheet).col_values(i)[0])
+    workbook_queryDict[workbook_query.sheet_by_name(objectiveSheet).col_values(i)[0]] = \
+        workbook_query.sheet_by_name(objectiveSheet).col_values(i)[1:]
+data = pd.DataFrame(workbook_queryDict, columns=colNames)
+            
+print(data.shape)
+lendingHelper = data.copy()
+
+
+objectiveSheet = 'ktcxhtxl'
+
+nRows, nCols = workbook_query.sheet_by_name(objectiveSheet).nrows, workbook_query.sheet_by_name(objectiveSheet).ncols
+colNames = []
+workbook_queryDict = {}
+for i in range(0, nCols):
+    colNames.append(workbook_query.sheet_by_name(objectiveSheet).col_values(i)[0])
+    workbook_queryDict[workbook_query.sheet_by_name(objectiveSheet).col_values(i)[0]] = \
+        workbook_query.sheet_by_name(objectiveSheet).col_values(i)[1:]
+data = pd.DataFrame(workbook_queryDict, columns=colNames)
+
+            
+print(data.shape)
+contactBook = data.copy()
+
+
+objectiveSheet = '月报中机构类型'
+# 之后 objectiveSheet 可以作为参数传进来。
+# workbook_querydata = pd.DataFrame(workbook_query) 不能直接转化成df
+nRows, nCols = workbook_query.sheet_by_name(objectiveSheet).nrows, workbook_query.sheet_by_name(objectiveSheet).ncols
+colNames = []
+workbook_queryDict = {}
+for i in range(0, nCols):
+    colNames.append(workbook_query.sheet_by_name(objectiveSheet).col_values(i)[0])
+    workbook_queryDict[workbook_query.sheet_by_name(objectiveSheet).col_values(i)[0]] = \
+        workbook_query.sheet_by_name(objectiveSheet).col_values(i)[1:]
+data = pd.DataFrame(workbook_queryDict, columns=colNames)
+            
+print(data.shape)
+dataCatagory = data.copy()
+# 后续考虑如何try exception，容纳几种常见错误。
